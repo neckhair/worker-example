6f8680ea4049a1cc433b7087f78d59782c935641
Philippe Hässig <philippe.haessig@sientia.ch>
2012-10-18 13:38:42 +0200

    working worker

--- app/controllers/worker_controller.rb
+++ app/controllers/worker_controller.rb
@@ -1,8 +1,11 @@
+
 class WorkerController < ApplicationController
   def index
   end
 
   def start
+    HardWorker.perform_async('bob', 5)
+    flash[:notice] = 'HardWorker started its work'
     redirect_to :root
   end
 end
--- app/workers/hard_worker.rb
+++ app/workers/hard_worker.rb
@@ -1,13 +1,35 @@
+class HardWorker
+  include Sidekiq::Worker
 
+  def perform(name, count)
+    primes = []
 
-module Workers
-
-  class HardWorker
-    include Sidekiq::Worker
-
-    def perform(name, count)
-      puts "#{name}'s doing hard work."
+    i = 0
+    while i < 1000000
+      primes << i if isPrime(i)
+      i += 1
     end
+
+    puts primes.inspect
   end
+
+  private
+
+    def isPrime(number)
+      if number == 0 or number == 1
+        return false
+      end
+
+      i = 2
+      limit = number / i
+      while i < limit
+        return false if number % i == 0
+
+        i += 1
+        limit = number / i
+      end
+
+      return true
+    end
 
 end
